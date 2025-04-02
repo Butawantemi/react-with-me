5fb8958e6b2ee2a4e54e25d85b66fd3224717fda
Butawantemi <japhetbuta@gmail.com>
2025-04-02 13:28:03 +0300

    getting start with react

---
+++ Tic-Tac-Toe/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+
+function MyButton () {
+  return (
+    <button>Hi there!</button>
+  );
+};
+
+
+export default function MyApp () {
+  return (
+    <>
+    <h1>Welcome to react!</h1>
+    <MyButton />
+    </>
+  );
+}
