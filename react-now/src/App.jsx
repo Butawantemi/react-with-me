bbc5a654104e305103a89dbc310eeb72bd4903d7
Butawantemi <japhetbuta@gmail.com>
2025-04-02 16:26:21 +0300

    usestate and props

--- react-now/src/App.jsx
+++ react-now/src/App.jsx
@@ -1,5 +1,53 @@
+import { useState } from "react";
 import "./App.css";
 
+//Updating the screen
+
+const MyButton = ({count, onClick}) => {
+  return (
+    <button onClick={onClick}>Clicked {count} times</button>
+  );
+
+};
+
+export default function MyApp() {
+  const [ count, setCount ] = useState(0);
+
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div className="container">
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
+    </div>
+  );
+}
+
+/* // Event handling 
+const MyButton = () => {
+  const handleClick = () => {
+    alert("You clicked me")
+ }
+
+  return (
+    <button onClick={handleClick}>Click</button>
+  );
+}
+
+export default function MyApp() {
+  
+  return (
+    <div>
+    <MyButton />
+    </div>
+  );
+} */
+
+/* 
+Rendering 
 const products = [
   { title: "Cabbage", isFruit: false, id: 1 },
   { title: "Garlic", isFruit: false, id: 2 },
@@ -23,4 +71,4 @@ export default function ShoppingList() {
       <ul>{listItems}</ul>
     </div>
   );
-}
+} */
