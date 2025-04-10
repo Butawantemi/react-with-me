e5fa6bfded3bb329704b56bc9c90e88961fcacfd
Butawantemi <japhetbuta@gmail.com>
2025-04-10 21:14:47 +0300

    image slider component

--- reactjs-interview-project/src/App.jsx
+++ reactjs-interview-project/src/App.jsx
@@ -1,22 +1,26 @@
 import Accordian from "./Component/accordian";
+import ImageSlider from "./Component/image-slider";
 import RandomColor from "./Component/random-color";
 import StarRating from "./Component/star-rating";
 
-
 export default function App() {
-
   return (
     <div>
       {/* Accordian component */}
       {/* <Accordian /> */}
 
       {/* Random color component */}
-     {/*  <RandomColor /> */}
+      {/*  <RandomColor /> */}
 
       {/* Star rating component */}
-      <StarRating noOfStar={10}/>
-    </div>
-  )
-}
+      {/* <StarRating noOfStar={10}/> */}
 
- 
+      {/* Image slider component */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
+    </div>
+  );
+}
