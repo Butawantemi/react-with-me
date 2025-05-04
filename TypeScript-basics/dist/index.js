86b780e42cd8a2ad1578065d48b0cb3fc74bcc05
Butawantemi <japhetbuta@gmail.com>
2025-05-04 18:37:43 +0300

    typescript

--- TypeScript-basics/dist/index.js
+++ TypeScript-basics/dist/index.js
@@ -1,5 +1,25 @@
 "use strict";
-const greeting = (massage) => {
-    return (`${massage} is greeting`);
+class userAccount {
+    constructor(name, id) {
+        this.name = name;
+        this.id = id;
+    }
+}
+const user = new userAccount("japhet", 1);
+console.log(user);
+const getLength = (obj) => {
+    return obj.length;
 };
-greeting("Hello");
+console.log(getLength("Japhet"));
+const wrapInArray = (obj) => {
+    if (typeof obj === "string") {
+        return [obj];
+    }
+    return obj;
+};
+console.log("Jackson" /* ["Japhet", "Josia", "Josephat", "Jovin"] */);
+const logPoint = (p) => {
+    console.log(`${p.x} ${p.y}`);
+};
+const point = { x: 20, y: 40 };
+logPoint(point);
--- TypeScript-basics/src/index.ts
+++ TypeScript-basics/src/index.ts
@@ -1,6 +1,49 @@
 
-const greeting = (massage: String) => {
-    return (`${massage} is greeting`)
+interface User {
+    id: number,
+    name: string
 }
 
-greeting("Hello")
+
+class userAccount {
+    name: string
+    id: number
+
+    constructor(name: string, id:number) {
+        this.name = name
+        this.id = id
+    }
+}
+
+
+const user : User = new userAccount("japhet", 1)
+
+console.log(user)
+
+const getLength = (obj : string | string[]) => {
+    return obj.length;
+}
+
+console.log(getLength("Japhet"))
+
+const wrapInArray = (obj: string | string[]) => {
+    if (typeof obj === "string") {
+        return [obj]
+    }
+
+    return obj
+}
+
+console.log("Jackson"/* ["Japhet", "Josia", "Josephat", "Jovin"] */)
+
+interface Point {
+    x: number
+    y: number
+}
+
+const logPoint = (p: Point) => {
+    console.log(`${p.x} ${p.y}`)
+}
+
+const point = {x: 20, y: 40}
+logPoint(point)
